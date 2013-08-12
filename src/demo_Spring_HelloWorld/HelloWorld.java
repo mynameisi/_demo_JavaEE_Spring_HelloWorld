6102d16986c7fb85a3ebdc998c9edaf946ac09f1
mynameisi <mynamestillisi@gmail.com>
2013-08-12 12:14:22 +0800

    http://www.tutorialspoint.com/spring/spring_hello_world_example.htm

---
+++ src/demo_Spring_HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package demo_Spring_HelloWorld;
+
+public class HelloWorld {
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+}
